b2bb666d63f5ee75537dbae75edf7042068eaa52
April Bates <a.madeline.bates@gmail.com>
2021-04-19 21:36:06 +0100

    adding notes to StagServer

--- src/StagServer.java
+++ src/StagServer.java
@@ -4,6 +4,9 @@ import java.util.*;
 
 class StagServer
 {
+    String entityFilename;
+    String actionFilename;
+
     public static void main(String args[])
     {
         if(args.length != 2) System.out.println("Usage: java StagServer <entity-file> <action-file>");
@@ -12,6 +15,8 @@ class StagServer
 
     public StagServer(String entityFilename, String actionFilename, int portNumber)
     {
+        this.entityFilename = entityFilename;
+        this.actionFilename = actionFilename;
         try {
             ServerSocket ss = new ServerSocket(portNumber);
             System.out.println("Server Listening");
@@ -39,6 +44,9 @@ class StagServer
 
     private void processNextCommand(BufferedReader in, BufferedWriter out) throws IOException
     {
+        //first call a class to parse entityFilename and actionFilename and return entity Graph and actions
+        //all other classes will walk through these graphs and pull correct data. data will also be changed +
+        //manipulated in these structs to present changes
         String line = in.readLine();
         out.write("You said... " + line + "\n");
     }
