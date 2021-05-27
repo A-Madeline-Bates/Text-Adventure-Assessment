e76c2b8500dbed7f8281b4e21285a3a443b34c8f
April Bates <a.madeline.bates@gmail.com>
2021-05-27 18:13:27 +0100

    Committing before adding multiplayer

---
+++ src/HandlePlayer.java
@@ -0,0 +1,15 @@
+//import data.PlayerState;
+//import tokeniser.Tokeniser;
+//
+//import java.util.ArrayList;
+//import java.util.List;
+//
+//public class HandlePlayer {
+//	final List<PlayerState> playerList = new ArrayList<PlayerState>();
+//
+//	public HandlePlayer(Tokeniser tokeniser){
+//		for()
+//	}
+//
+//
+//}
